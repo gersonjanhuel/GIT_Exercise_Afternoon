60e40f1752e5ea76c01190682201548140744fc4
Gerson Janhuel <gersonjanhuel@gmail.com>
2023-05-26 15:40:49 +0700

    Initial Commit

---
+++ GIT_Exercise_Afternoon/GIT_Exercise_AfternoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIT_Exercise_AfternoonApp.swift
+//  GIT_Exercise_Afternoon
+//
+//  Created by Gerson Janhuel on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GIT_Exercise_AfternoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
